cb97ef72c4ea5ebc5b45f7fa6089f2e6258ac671
José Contreras <joseleonardocontreras00@gmail.com>
2022-03-06 09:37:01 -0400

    Arreglado pequeño error con la fecha en adaptador

--- src/flight/FlightPostgreAdapter.ts
+++ src/flight/FlightPostgreAdapter.ts
@@ -13,12 +13,13 @@ export class FlightPostgreAdapter implements FlightRepo {
     private mapper: FlightMapper,
   ) {}
 
-  async getFiltered(filtros: FilterBody): Promise<FlightDTO[]> {
+  async getFiltered(filters: FilterBody): Promise<FlightDTO[]> {
+    const formatedDate: Date = new Date(filters.date);
     const flights = await this.flightRepository.find({
       where: {
-        origin: filtros.origin,
-        destination: filtros.destination,
-        fligthDate: filtros.date,
+        origin: filters.origin,
+        destination: filters.destination,
+        fligthDate: formatedDate.toISOString(),
       },
     });
     return flights.map((flight) => this.mapper.entityToDto(flight));
